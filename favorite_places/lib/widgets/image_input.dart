ad3355e3eac21edb19a96abe9ff0f7486e2641cd
Manav Pande <manavpande2005@gmail.com>
2025-01-19 10:15:27 +0530

    TakePicSolve

---
+++ favorite_places/lib/widgets/image_input.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+  @override
+  State<ImageInput> createState() {
+    return _ImageInputState();
+  }
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      //here Xfile of pickedImage is converted into normal file
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+      onPressed: _takePicture,
+    );
+
+    if (_selectedImage != null) {
+      content = Image.file(
+        _selectedImage!,
+        fit: BoxFit.cover,
+        width: double.infinity,
+      );
+    }
+
+    return Container(
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+        ),
+      ),
+      child: content,
+    );
+  }
+}
